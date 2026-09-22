184205b381fefadb5dc9aa59fd3fe609633bc4d6
Regina Moiseeva <moiseevaryu@gmail.com>
2026-09-22 17:24:02 +0500

    feat: finish navbar menu & add photos

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import AOS from 'aos'
 import 'aos/dist/aos.css'
 
 import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 
 const App = () => {
   const [darkMode, setDarkMode] = useState(true)
@@ -33,6 +34,7 @@ const App = () => {
       `bg-linear-to-br from-gray-50 to-red-50 min-h-screen`
     }>
       <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+      <Hero/>
     </div>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,6 +9,7 @@ import {
 } from 'react-icons/fa';
 
 import { motion } from 'framer-motion';
+import { Moon, Sun } from 'lucide-react';
 
 interface NavbarProps {
   darkMode: boolean;
@@ -35,9 +36,62 @@ const Navbar: React.FC<NavbarProps> = ({ darkMode, toggleDarkMode }) => {
           transition={{ duration: 0.5 }}
           className='relative w-[95%] max-w-4xl mb-4'
         >
-          <div className='relative bg-linear-to-r from-red-600 to red-800
-          backdrop-blur-xl rounded-2xl shadow-2xl border border-white/20 px-3 py-2'>
-            <div className="absolute -top-5 right-3"></div>
+          <div className='relative bg-linear-to-r from-red-600 to-red-800 backdrop-blur-xl rounded-2xl shadow-2xl border border-white/20 px-3 py-2'>
+            <div className="absolute -top-5 right-3">
+              <motion.button
+                whileHover={{ scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+                onClick={toggleDarkMode}
+                className='p-2 rounded-full bg-gray-900 dark:bg-gray-100 transition-colors backdrop-blur-sm'
+              >
+                { 
+                  darkMode ? 
+                  ( <Sun className='w-5 lg:w-5 h-5 lg:h-5 text-black'/>) : 
+                  (<Moon className='w-5 lg:w-5 h-5 lg:h-5  text-white'/>)
+                }
+              </motion.button>
+            </div>
+            <div className='flex items-center justify-around gap-1'>
+                {
+                  navItems.map((item) => {
+                    const Icon = item.icon
+                    const isActive = activeTab === item.name
+                    return (
+                      <motion.a
+                        key={item.name}
+                        href={item.link}
+                        onClick={() => setActiveTab(item.name)}
+                        whileHover={{ scale: 1.1 }}
+                        whileTap={{ scale: 0.9 }}
+                        className='flex flex-col items-center gap-0.5 py-1.5 px-2 relative group flex-1'
+                      >
+                        {
+                          isActive && 
+                          (
+                            <motion.div 
+                              layoutId='activeTab' 
+                              className='absolute -top-2 left-1/2 -translate-x-1/2 w-6 h-1 bg-white rounded-full'
+                              transition={{ duration: 0.3 }}
+                            >
+                            </motion.div>
+                          )
+                        }
+                        <Icon
+                          className={`w-5 h-5 transition-all duration-300 
+                            ${isActive ? 'text-white' : 'text-white/60 group-hover:text-white'}`
+                          }
+                        />
+
+                        <span className={`text-[10px] font-medium transition-all duration-300
+                          ${ isActive ? 'text-white' : 'text-white/60 group-hover:text-white'}`}
+                        >
+                          {item.name}
+                        </span>
+                      </motion.a>
+                    )
+                  })
+                }
+            </div>
           </div>
         </motion.nav>
       </div>
